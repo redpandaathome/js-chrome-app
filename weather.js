9df94f3ac69ebba30ef98f1762315d42b853013f
redpandaathome <leeyumi415@gmail.com>
2020-08-10 13:51:35 +0900

    added weather api

--- weather.js
+++ weather.js
@@ -1,5 +1,18 @@
 
+const weather = document.querySelector(".js-weather");
+const API_KEY = "7aee812ef794c947690b4bcdc75778bd";
 const COORDS = 'corrds';
+
+function getWeather(lat, long){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metrics`
+    ).then(function(response){
+        return response.json()
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    })
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +26,7 @@ function handleGeoSuccess(position){
         longitude
     }
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -24,11 +38,13 @@ function askForCoords(){
 }
 
 function loadCoords(){
-    const loadedCords = localStorage.getItem(COORDS);
-    if(loadedCords == null){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords == null){
         askForCoords();
     } else {
         //get weather
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
